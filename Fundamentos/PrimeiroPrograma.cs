41f3d2097173ff7e56e131bb45d3c7f8e8f54d77
EltonDutradias <elton.dutradias.ti@gmail.com>
2021-06-29 22:38:04 -0300

    Add project files.

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+        static void Main() { 
+            /* Azul: Palavras reservadas da linguagem, Cinza: Indica que a palavra digitada não pe necessária. 
+             * Uso do System no using 
+             * Main: Método que é porta de entrada de programa CSharp. 
+             * Problemas múltiplos método Main definido. 1° Solução: Alterar o nome do método toda vez que criar um novo.  
+             * 2° Solução: Botão direito mouse -> propriedades -> aplicativos -> objetos de inicialização -> definir o método que pretende executar.
+             * 
+             */
+            System.Console.Write("Primeiro");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }
+    }
+}
